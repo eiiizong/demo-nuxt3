a7d51ac84f1db47ecc83c635283c51b837a822ef
eiiizong <eiiizong@163.com>
2023-01-07 16:03:04 +0800

    框架搭建：引入 element-plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: './src',
+  // 全局引入element-plus样式
+  css: ['element-plus/dist/index.css'],
   vite: {
     css: {
       preprocessorOptions: {
