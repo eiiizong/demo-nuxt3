f8e64b4bce157d88b82ceb09af7c475785bf1440
eiiizong <eiiizong@163.com>
2023-01-07 19:49:37 +0800

    框架搭建

---
+++ src/layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error">error</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  error: {
+    type: Object,
+  },
+})
+
+console.log(props, 987)
+</script>
+
+<style lang="scss" scoped>
+.error {
+  width: 100%;
+  height: 100%;
+}
+</style>
