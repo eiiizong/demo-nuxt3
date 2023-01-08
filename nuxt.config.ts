743409dba8615fee8f641cab936c1a537f0c2502
eiiizong <eiiizong@163.com>
2023-01-08 15:39:24 +0800

    框架搭建

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
+// import AutoImport from 'unplugin-auto-import/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: './src',
   // 全局引入element-plus样式
   css: ['normalize.css/normalize.css', 'element-plus/dist/index.css'],
-  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
+  modules: [['@pinia/nuxt'], '@pinia-plugin-persistedstate/nuxt'],
   app: {
     head: {
       title: '蓉易贷',
     },
   },
+
   vite: {
     css: {
       preprocessorOptions: {
